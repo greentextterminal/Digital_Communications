78d02ea6389641faf9663458ddd0c20c77ad8224
greentextterminal <68518334+greentextterminal@users.noreply.github.com>
2023-05-11 02:14:13 -0500

    Delete ECEN455Labs directory

--- ECEN455Labs/Lab2/binomial_prob.m
+++
@@ -1,5 +0,0 @@
-%% Binomial Probability Function 
-function prob=binomial_prob(M,k,p)
-    binomial_coeff = nchoosek(M,k);
-    prob=binomial_coeff * (p^k) * (1 - p)^(M-k) ;
-end
--- ECEN455Labs/Lab2/geometric_prob.m
+++
@@ -1,3 +0,0 @@
-function prob=geometric_prob(k,p)
-    prob = (1-p) ^ (k-1) * p;
-end
